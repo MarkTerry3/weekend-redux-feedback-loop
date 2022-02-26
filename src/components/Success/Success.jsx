9e294b6b6468c35915480669aba55aae3e60d1c3
MarkTerry3 <mark.terry93@gmail.com>
2022-02-26 17:40:28 -0600

    all functionality done

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,12 +1,19 @@
-
+import { useHistory } from 'react-router-dom';
 
 
 
 function Success() {
+
+    const history = useHistory();
+
+    const newFeedback = () => {
+        history.push('/');
+    }
+
     return(
         <>
             <h3>thanks...I guess</h3>
-            <button>Leave New Feedback!</button>
+            <button onClick={newFeedback}>Leave New Feedback!</button>
         </>
     )
 }// end Success
