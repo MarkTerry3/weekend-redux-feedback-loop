756bcebb4f93fbcc7d60397161974e34ec81f383
MarkTerry3 <mark.terry93@gmail.com>
2022-02-26 15:34:12 -0600

    review page loading correctly

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,14 +1,25 @@
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useState} from 'react';
 import {Provider} from 'react-redux';
 
 
 
-
 function ReviewFeedback() {
+
+
+    const feelingReducer = useSelector(store => store.feelingReducer);
+    const understandingReducer = useSelector(store => store.understandingReducer);
+    const supportReducer = useSelector(store => store.supportedReducer);
+    const commentsReducer = useSelector(store => store.commentsReducer);
+
+
     return(
         <>
-            <p>hyello reviewFeedback</p>
+            <h1>Review Your Feedback</h1>
+            <p>Feeling: {feelingReducer}</p>
+            <p>Understanding: {understandingReducer}</p>
+            <p>Support: {supportReducer}</p>
+            <p>Comments: {commentsReducer}</p>
         </>
     )
 }
